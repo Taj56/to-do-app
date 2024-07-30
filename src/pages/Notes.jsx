37c87d36bfc7b5576c03563c25ab86e704ae30ce
Taj56 <64923628+Taj56@users.noreply.github.com>
2024-07-29 19:41:18 -0500

    basic notes render

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,7 +1,30 @@
-
+import { useEffect, useState } from "react"
+import { databases } from "../appwrite/config"
 const Notes = () => {
+
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        init()
+    }, [])
+
+    const init = async () => {
+        const res = await databases.listDocuments(
+            import.meta.env.VITE_DATABASE_ID,
+            import.meta.env.VITE_COLLECTION_ID_NOTES
+        );
+
+        setNotes(res.documents);
+    }
+
   return (
-    <div>Notes</div>
+    <div>
+        <div>
+            {notes.map((note) => (
+                <div key={note.$id}>{note.body}</div>
+            ))}
+        </div>
+    </div>
   )
 }
 export default Notes
